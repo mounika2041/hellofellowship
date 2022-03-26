b2f5c49b0d338ca3260709f1505ea4b90aa4de17
duvvamounika <mounikaduvva05@gmail.com>
2022-03-26 22:00:51 +0530

    first commit

---
+++ OOPS-CONCEPTS/ClassObject/Fruit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_CONCEPTS.ClassObject
+{
+    class Fruit
+    {
+        //instance variable
+        public string name, colour;
+
+        public Fruit(string name, string colour)
+        {
+            this.name = name;
+            this.colour = colour;
+        }
+        public void FruitDetails()
+        {
+            Console.WriteLine("fruit name:{0} colour:{1}", name, colour);
+        }
+    }
+}
+    
+
---
+++ OOPS-CONCEPTS/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_CONCEPTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit apple = new Fruit("apple", "red");
+            apple.fruitDetails();
+            Fruit orange = new Fruit("orange", "yellow");
+            orange.fruitDetails();
+            Console.ReadLine();
+        }
+    }
+
+    class Fruit
+    {
+        public Fruit(string v1, string v2)
+        {
+        }
+
+        internal void fruitDetails()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
